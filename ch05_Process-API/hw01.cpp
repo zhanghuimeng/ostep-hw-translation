c40eb404767119c5e8cf8bb05fe02ef88db7dc14
zhanghuimeng <zhanghuimeng1997@gmail.com>
2018-07-07 16:58:45 +0800

    【新增】makefile

--- ch05_Process-API/hw01.cpp
+++ ch05_Process-API/hw01.cpp
@@ -7,19 +7,19 @@ main(int argc, char *argv[])
 {
     int x;
     x = 100;
-    printf("x = %d (pid:%d)\n", &x, (int) getpid());
+    printf("x = %d (pid:%d)\n", x, (int) getpid());
     int rc = fork();
     if (rc < 0) { // fork failed; exit
         fprintf(stderr, "fork failed\n");
         exit(1);
     } else if (rc == 0) { // child (new process)
-        printf("child: x = %d (pid:%d)\n", &x, (int) getpid());
+        printf("child: x = %d (pid:%d)\n", x, (int) getpid());
         x = 99;
-        printf("child: x = %d (pid:%d)\n", &x, (int) getpid());
+        printf("child: x = %d (pid:%d)\n", x, (int) getpid());
     } else { // parent goes down this path (main)
-        printf("parent: x = %d (pid:%d)\n", &x, (int) getpid());
+        printf("parent: x = %d (pid:%d)\n", x, (int) getpid());
         x = 101;
-        printf("parent: x = %d (pid:%d)\n", &x, (int) getpid());
+        printf("parent: x = %d (pid:%d)\n", x, (int) getpid());
     }
     return 0;
 }
